ec856ea1e7812f24b7670de79ce7bd071ec7845f
Greg <gporceng18@Gmail.com>
2021-07-23 10:48:56 -0400

    Fixed screen title and added game screen

--- Linux/main2.py
+++ Linux/main2.py
@@ -19,12 +19,14 @@ Builder.load_string("""
         Image:
             source: "logo.png"
             pos_hint: {'center_x':0.5, 'center_y':0.65}
+            
 
 
         MDFillRoundFlatButton:
             text:"start"
             font_size: '32sp'
             pos_hint: {'center_x':0.5,'center_y':0.4}
+            on_press: root.manager.current = 'game'
             size_hint: (.5,.1)
 
         
@@ -51,8 +53,8 @@ Builder.load_string("""
             pos_hint: {'center_x':0.5,'center_y':0.6}
             halign: "center"
             font_style: 'H2'
-            them_text_color: "Custom"
-            text_color: 0,0,1,1
+            theme_text_color: "Custom"
+            text_color: 1,1,1,1
 
 
         MDSlider:
@@ -62,11 +64,38 @@ Builder.load_string("""
 
         
         MDFillRoundFlatButton:
-            text:"back"
+            text:"save"
             font_size: '32sp'
             pos_hint: {'center_x':0.5,'center_y':0.4}
             on_press: root.manager.current = 'menu'
             size_hint: (.5,.1)
+
+        MDFillRoundFlatButton:
+            text:"back"
+            font_size: '32sp'
+            pos_hint: {'center_x':0.5,'center_y':0.2}
+            on_press: root.manager.current = 'menu'
+            size_hint: (.5,.1)
+
+<GameScreen>:
+    Screen:
+        Image:
+            source: "bg.png"
+            size: self.size
+            pos: self.pos
+            size: root.size
+            allow_stretch: True
+            keep_ratio: False
+
+        MDLabel:
+            text: "Game Started"
+            font_size: '32sp'
+            pos_hint: {'center_x':0.5,'center_y':0.9}
+            halign: "center"
+            font_style: 'H2'
+            theme_text_color: "Custom"
+            text_color: 1,1,1,1
+
             
 """)
 
@@ -76,16 +105,20 @@ class MenuScreen(Screen):
 class SettingsScreen(Screen):
     pass
 
+class GameScreen(Screen):
+    pass
+
 
 class design(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Red"
         self.theme_cls.primary_hue = "50"
-        self.title = ("Simple 5 Start Menu Design - By Gregory Porceng")
+        self.title = ("Simon Game - By Gregory Porceng")
 
         sm = ScreenManager(transition=FadeTransition())
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(GameScreen(name='game'))
         return sm
 
 
